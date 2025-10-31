a75f7b22d65bba2c96d5cc7ab4ce7fa6682bee18
Dominik Toton <dominik.toton@iohk.io>
2025-10-31 14:03:26 +0100

    fix: allow to use browser cryptography when in wasm

--- cryptography/lib/src/helpers/random_bytes.dart
+++ cryptography/lib/src/helpers/random_bytes.dart
@@ -20,7 +20,7 @@ import 'package:cryptography_plus/helpers.dart';
 import '../../cryptography_plus.dart';
 
 export 'random_bytes_impl_default.dart'
-    if (dart.library.html) 'random_bytes_impl_browser.dart';
+    if (dart.library.js_interop) 'random_bytes_impl_browser.dart';
 
 const _hexChars = [
   '0',
