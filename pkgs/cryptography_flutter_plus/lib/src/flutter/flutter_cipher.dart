9fb5c22c766aabf73f59fd4c9c9fe276163e4537
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2026-05-29 13:14:11 +0200

    Added documentation to close https://github.com/emz-hanauer/dart-cryptography/issues/23
    Changing the function signature to `Uint8List clearText` was not possible due to the type `List<int>` being required for different (e.g. browser) implementations.

--- pkgs/cryptography_flutter_plus/lib/src/flutter/flutter_cipher.dart
+++ pkgs/cryptography_flutter_plus/lib/src/flutter/flutter_cipher.dart
@@ -298,6 +298,13 @@ mixin FlutterCipherMixin
     );
   }
 
+  /// Encrypts a cleartext.
+  ///
+  /// The [clearText] will be converted to a [Uint8List] before encryption. If
+  /// the [clearText] contains any non-8-bit values, they will be truncated to 8
+  /// bits. See [Uint8List] for details.
+  ///
+  /// For other arguments, see [StreamingCipher.encrypt] and [Cipher.encrypt].
   @override
   Future<SecretBox> encrypt(
     List<int> clearText, {
